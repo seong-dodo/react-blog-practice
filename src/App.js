100f93c2b4bff521667a366fa369091f222fc1bc
seong-dodo <68280407+seong-dodo@users.noreply.github.com>
2020-09-06 23:30:43 +0900

    모달숙제(열고닫기),반복문

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ function App() {
   let posts = '강남 고기 맛집';
 
   let [modal, modal변경] = useState( false )
+  let [newpost, newpost작성] = useState( false )
+
+  var 어레이 =[2,3,4] ;
+  어레이.map(function(a) {
+   return a*10
+  });
+
 
 
   function 제목바꾸기(){
@@ -28,6 +35,12 @@ function App() {
     
   }
 
+  function newpost열기닫기(){
+    //만약에 newPost가 true 이면 false로 업데이트
+    if(newpost == true) newpost작성(false)
+    else newpost작성(true)
+    //만약에 newPost가 false 이면 true로 업데이트
+  }
   
   return (
     <div className="App">
@@ -45,10 +58,31 @@ function App() {
         <hr/>
       </div> 
       <div className="list">
-        <h3 onClick={()=>{modal변경(true)}}>{ 글제목[2] }<button onClick={ 제목바꾸기정렬 }>가나다순정렬</button></h3>
+        <h3 onClick={()=>{modal변경(!modal)}}>{ 글제목[2] }<button onClick={ 제목바꾸기정렬 }>가나다순정렬</button></h3>
         <p>2월19일 발행</p>
         <hr/>
+        <button onClick={()=>{newpost열기닫기()}}>새글작성</button>
       </div> 
+
+
+     {
+       글제목.map(function (글) {
+         return (
+          <div className="list">
+           <h3>{ 글 }<span onClick={()=>{좋아요변경(좋아요+1)}}>♡</span>{ 좋아요 }</h3>
+           <p>2월17일 발행</p>
+           <hr/>
+          </div> 
+         )
+      })
+      
+     }
+
+     {
+       newpost === true
+       ? <Newpost />
+       : null
+     }
 
     {
       modal === true
@@ -60,6 +94,14 @@ function App() {
   );
 }
 
+function Newpost(){
+ return(
+   <div className="newpost">
+     <h2>POST제목</h2>
+     <p>새글작성하기</p>
+   </div>
+ ) 
+}
 
 function Modal(){
 return(
