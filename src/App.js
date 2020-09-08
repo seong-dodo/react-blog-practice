cc768ef218265fb9e7a65f95280b520dd09a725d
seong-dodo <68280407+seong-dodo@users.noreply.github.com>
2020-09-08 14:21:09 +0900

    props응용 -상세페이지

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import './App.css';
 function App() {
  
   let [글제목, 글제목변경] = useState( ['할아버지 코트 추천','여자 코트 추천','유행하는 아이템'] );
-  let [좋아요, 좋아요변경] = useState(0)
+  let [좋아요, 좋아요변경] = useState(0);
   let posts = '강남 고기 맛집';
 
-  let [modal, modal변경] = useState( false )
-  let [newpost, newpost작성] = useState( false )
+  let [modal, modal변경] = useState( false );
+  let [newpost, newpost작성] = useState( false );
+  let [누른제목, 누른제목변경] = useState(0);
 
   var 어레이 =[2,3,4] ;
   어레이.map(function(a) {
@@ -58,18 +59,23 @@ function App() {
         <hr/>
       </div> 
       <div className="list">
-        <h3 onClick={()=>{modal변경(!modal)}}>{ 글제목[2] }<button onClick={ 제목바꾸기정렬 }>가나다순정렬</button></h3>
+        <h3 onClick={ ()=>{modal변경(!modal)} }>{ 글제목[2] }<button onClick={ 제목바꾸기정렬 }>가나다순정렬</button></h3>
         <p>2월19일 발행</p>
         <hr/>
-        <button onClick={()=>{newpost열기닫기()}}>새글작성</button>
+
+      {/*  <button onClick={ ()=>{ 누른제목변경(0) } }>버튼1</button>
+        <button onClick={ ()=>{ 누른제목변경(1) } }>버튼2</button>
+        <button onClick={ ()=>{ 누른제목변경(2) } }>버튼3</button> */}
+
+        <button onClick={ ()=>{ newpost열기닫기() } }>새글작성</button>
       </div> 
 
 
      {
-       글제목.map(function (글) {
+       글제목.map(function (글, i) {
          return (
           <div className="list">
-           <h3>{ 글 }<span onClick={()=>{좋아요변경(좋아요+1)}}>♡</span>{ 좋아요 }</h3>
+           <h3 onClick={ ()=>{ 누른제목변경(i) } }>{ 글 }<span onClick={ ()=>{좋아요변경(좋아요+1)} }>♡</span>{ 좋아요 }</h3>
            <p>2월17일 발행</p>
            <hr/>
           </div> 
@@ -80,13 +86,14 @@ function App() {
 
      {
        newpost === true
-       ? <Newpost />
+       ? <Newpost 글제목={ 글제목 } 누른제목={ 누른제목 } ></Newpost>
        : null
      }
 
+
     {
       modal === true
-      ? <Modal 글제목={글제목}></Modal>
+      ? <Modal 글제목={ 글제목 } ></Modal>
       : null
     }
     
@@ -94,10 +101,10 @@ function App() {
   );
 }
 
-function Newpost(){
+function Newpost(props){
  return(
    <div className="newpost">
-     <h2>POST제목</h2>
+     <h2>{ props.글제목[props.누른제목] }</h2>
      <p>새글작성하기</p>
    </div>
  ) 
@@ -106,7 +113,7 @@ function Newpost(){
 function Modal(props){
 return(
    <div className="modal">
-      <h2>{props.글제목[0]}</h2>
+      <h2>{ props.글제목[0] }</h2>
       <p>날짜</p>
       <p>상세내용</p>
    </div>
